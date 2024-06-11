b7d0a5db3dd4bfe146c88db7f1da0d8d0ef884c4
Oluwayomi Felix <77457879+Portia-dot@users.noreply.github.com>
2024-06-10 23:44:43 -0600

    Initial Commit

---
+++ WalletApp/WalletAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletAppApp.swift
+//  WalletApp
+//
+//  Created by Modamori Oluwayomi on 2024-06-10.
+//
+
+import SwiftUI
+
+@main
+struct WalletAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
